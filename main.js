57c2a13e7cb06ef64bb369f22de95409ec588c28
Rovshan Alizada <rovshanalizada@gamil.com>
2024-09-08 01:32:31 +0400

    done

---
+++ main.js
@@ -0,0 +1,74 @@
+//header
+
+window.addEventListener("scroll", () => {
+    let header = document.querySelector(".header-bottom")
+    if (window.scrollY > 200) {
+        header.classList.add("headerAnime")
+    } else {
+        header.classList.remove("headerAnime")
+    }
+})
+
+
+// section3
+
+let valueDisplays = document.querySelectorAll(".animated-first")
+let interval = 2000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"))
+    let duration = Math.floor(interval / endValue)
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter)
+        }
+    }, duration)
+
+})
+
+
+//search
+
+const searchIcon = document.getElementById('btnSearch');
+const searchOverlay = document.getElementById('searchOverlay');
+
+searchIcon.addEventListener('click', function () {
+    searchOverlay.style.display = 'flex'; 
+});
+
+searchOverlay.addEventListener('click', function (e) {
+    if (e.target === searchOverlay) {
+        searchOverlay.style.display = 'none'; 
+    }
+});
+
+
+//choose language
+
+document.getElementById('lang').addEventListener('click', () => {
+console.log("clicked");
+
+})
+
+
+
+
+// Wait for the page to fully load
+window.addEventListener('load', () => {
+    const loadingOverlay = document.getElementById('loadingOverlay');
+});
+
+
+
+
+
+document.getElementById('langBtn').addEventListener('click', function() {
+    const dropdown = document.getElementById('dropdownMenu');
+    dropdown.classList.toggle('show');
+});
+
+
+
